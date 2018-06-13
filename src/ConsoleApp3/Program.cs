41e3ec88b42ec983cb271f4867e3b39c25f6035b
Marc Gravell <marc.gravell@gmail.com>
2018-06-13 13:31:26 +0100

    add exe

---
+++ src/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using PlatformDynamicMethod;
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        // THIS IS ALL NONSENSE - I fucked up and took a ref we can't use; it is garbage
+        static void Main(string[] args)
+        {
+
+            ClassLibrary1.Class1.FromLib();
+
+            var type = ClassLibrary1.Class1.CreateType();
+            var obj = Activator.CreateInstance(type);
+            Console.WriteLine(obj);
+
+            var factory = DynamicMethodFactory.Create<Func<string>>("MyCode");
+
+            
+            var il = factory.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "from app");
+            il.Emit(OpCodes.Ret);
+
+            var method = factory.CreateDelegate();
+
+            Console.WriteLine(method());
+        }
+    }
+}
